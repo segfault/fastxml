2740087659a81e74e8fe3023933e72d21b5052ce
Mark Guzman <segfault@hasno.info>
2007-07-01 05:21:15 +0000

    makefile now looks up xml libraries
    git-svn-id: svn://hasno.info/fastxml/trunk@5 b3082176-f867-4bde-be85-e3c57d66f029

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,7 @@
 require 'mkmf'
 ext_name = 'fastxml'
 dir_config ext_name
+find_header( 'libxml/tree.h', "/usr/include/libxml2", "/usr/local/include/libxml2", "/opt/local/include/libxml2"  )
+find_library( 'xml2', "xmlInitParser", "/usr/lib", "/usr/local/lib", "/opt/local/lib" )
+have_library( 'xml2', 'xmlInitParser', 'libxml/parser.h' )
 create_makefile ext_name
